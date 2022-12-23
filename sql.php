9a56d88070dfdef65bf0a3f8310a616172c97826
Young-Way <young-way.tsan@etu.iut-tlse3.fr>
2022-12-23 18:35:34 +0100

    Login

---
+++ sql.php
@@ -0,0 +1,33 @@
+<?php
+class requeteSQL {
+
+    private $linkpdo;
+
+    public function __construct()
+    {
+        ///Connexion au serveur MySQL avec PDO
+        $server = 'localhost';
+        $login  = 'root';
+        $mdp    = '';
+        $db     = 'iutprojetphp';
+
+        try {
+            $this->linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+            $this->linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        ///Capture des erreurs éventuelles
+        catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    public function getJoueur(){
+        $result = array();
+        $req = $this-> linkpdo ->prepare("SELECT nom FROM joueur");
+        $testreq = $req->execute();
+        return $req;
+    }
+
+}
+
+?>
