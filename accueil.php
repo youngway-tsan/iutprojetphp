07ec48f2dd997f33f0cc6e6a0888a1f60f78796c
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2022-12-24 16:10:23 +0100

    page d'accueil et header

---
+++ accueil.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Accueil - F.C. WOIPPY</title>
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" href="img/FCWoippy-logo.png">
+    </head>
+
+
+    <header>
+        <div class="topnavbar">
+            <a class="header-logo-link" href="accueil.php">
+                <img src="img/FCWoippy-logo.png" alt="" class="navbar-logo">
+            </a>
+            <ul class="navbar-menu">
+                <li class="navbar-item"><a href="accueil.php" class="navbar-link">Accueil</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Joueurs</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Matchs</a></li>
+                <li class="navbar-item"><a href="#" class="navbar-link">Statistiques</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <body>
+    <main>
+        <div class="home-title">
+            <h1>Bienvenue Manager !</h1>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue sit amet lacus et maximus. Morbi elementum commodo justo sed posuere. Donec a vestibulum quam. Nulla tempor, lectus non rhoncus fringilla, nisi urna placerat eros, in commodo sem nibh sed diam. Fusce aliquet massa sed orci imperdiet, a sodales nulla commodo. Curabitur quis lorem nulla. Mauris id ipsum magna.</span>
+        </div>
+    </main>
+    </body>
+
+
+</html>
