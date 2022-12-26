7c72946f92b6ac3a3699b4f33ed8c0bd90faf43f
Young-Way <young-way.tsan@etu.iut-tlse3.fr>
2022-12-26 15:50:21 +0100

    Login finit + création des pages du sous-menu

---
+++ listematch.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Liste des matchs - F.C. WOIPPY</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" href="img/FCWoippy-logo.png">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
