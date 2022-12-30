9fea975fbb323dc927f7225b9930a995f95bb225
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2022-12-30 12:53:14 +0100

    ptit réglages sur header accueil

--- accueil.php
+++ accueil.php
@@ -4,7 +4,6 @@
         <meta charset='UTF-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Accueil - F.C. WOIPPY</title>
-        <link rel="stylesheet" href="css/index.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="icon" href="img/FCWoippy-logo.png">
     </head>
