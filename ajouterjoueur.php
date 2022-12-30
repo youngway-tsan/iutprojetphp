e4074d18fde29ab68488e531df964e70381763be
Young-Way <young-way.tsan@etu.iut-tlse3.fr>
2022-12-30 12:44:08 +0100

    Liste joueur + ajouter joueur

--- ajouterjoueur.php
+++ ajouterjoueur.php
@@ -8,33 +8,6 @@
         <link rel="icon" href="img/FCWoippy-logo.png">
     </head>
 
-
-    <header>
-        <div class="topnavbar">
-            <a class="header-logo-link" href="accueil.php">
-                <img src="img/FCWoippy-logo.png" alt="" class="navbar-logo">
-            </a>
-            <div class="navbar-menu-div">
-            <ul class="navbar-menu">
-                <li class="navbar-item"><a href="accueil.php" class="navbar-link">Accueil</a></li>
-                <li class="navbar-item"><a href="#" class="navbar-link">Joueurs</a>
-                    <ul class="sousmenu">
-                        <li class="navbar-item"><a href="ajouterjoueur.php" class="navbar-link">Ajouter un joueur</a></li>
-                        <li class="navbar-item"><a href="listejoueur.php" class="navbar-link">Liste des joueurs</a></li>
-                    </ul>
-                </li>
-                <li class="navbar-item"><a href="#" class="navbar-link">Matchs</a>
-                    <ul class="sousmenu">
-                        <li class="navbar-item"><a href="ajoutermatch.php" class="navbar-link">Ajouter un match</a></li>
-                        <li class="navbar-item"><a href="listematch.php" class="navbar-link">Liste des matchs</a></li>
-                    </ul>
-                </li>
-                <li class="navbar-item"><a href="#" class="navbar-link">Statistiques</a></li>
-            </ul>
-            </div>
-        </div>
-    </header>
-
     <?php
         require_once("header.php");
         $header = new header();
