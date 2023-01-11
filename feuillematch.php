d1a6184e204e75ae811cdf0b39abc55c9c4031b8
Theo Barat <theo.barat@etu.iut-tlse3.fr>
2023-01-11 19:32:38 +0100

    continuation feuillematch.php

--- feuillematch.php
+++ feuillematch.php
@@ -28,18 +28,46 @@
         <main class="main-listes">
             <section class="main-listes-container">
                 <h1>Feuille de match : <?php echo $nomAdversaire ?> [<?php echo $lieu ?>] </h1>
-                <hr>
-                <form action="" method="post">
-                    
-                </form>
+                </br>
 
-                <table>
-                    <?php
-                        echo '
-                        <td>'.$nomAdversaire.'</td>';
+                <svg  width="100%" height="100%" id="svg" viewBox="0 0 1150 820" xmlns="http://www.w3.org/2000/svg">
+
+                    <image  height="1220" id="_field" width="1150" x="0" y="0" xlink:href="https://www.demivolee.com/wp-content/plugins/OutilCompoWordpress-2.7.0/assets/field/field_big.png"></image>
+                    <image  height="790" id="_lines" width="1150" x="0" y="20" xlink:href="https://www.demivolee.com/wp-content/plugins/OutilCompoWordpress-2.7.0/assets/field/lines.png"></image>
+
+                    <g _ngcontent-ljs-c23="" left="0" top="0" transform="matrix(1,0,0,1,0,20)" class="contents">
                         
-                    ?>         
-                </table>
+        
+                        <g _ngcontent-ljs-c23="" id="g_j0" transform="matrix(1,0,0,1,120,340)" app-goalkeeper-field="" class="gardien g_player" _nghost-ljs-c21="">
+                            <image _ngcontent-ljs-c21="" height="70" left="0" preserveAspectRatio="none" top="0" width="70" x="0" y="0" xmlns:svg="http://www.w3.org/2000/svg" class="m_gardien dropzone" xlink:href="https://www.demivolee.com/wp-content/plugins/OutilCompoWordpress-2.7.0/assets/maillot/neutre.png" style="/* filter: none; */"></image>
+                            <g _ngcontent-ljs-c21="" id="drag_j0" xmlns:svg="http://www.w3.org/2000/svg" class="dropzone draggable">
+                                <rect _ngcontent-ljs-c21="" fill="#21316a" height="39" rx="10" ry="10" width="80" x="-5" y="69"></rect>
+                                <text  fill="#ffffff" id="j0" left="0" top="0" transform="matrix(1,0,0,1,-7.5, 97)" class="text_joueur" style="font-size: 24px; font-family: Arial; text-anchor: start;"></text>
+                             </g>
+                        </g>
+
+                        <g _ngcontent-ljs-c23="" id="g_j0" transform="matrix(1,0,0,1,300,100)" app-goalkeeper-field="" class="gardien g_player" _nghost-ljs-c21="">
+                            <image _ngcontent-ljs-c21="" height="70" left="0" preserveAspectRatio="none" top="0" width="70" x="0" y="0" xmlns:svg="http://www.w3.org/2000/svg" class="m_gardien dropzone" xlink:href="https://www.demivolee.com/wp-content/plugins/OutilCompoWordpress-2.7.0/assets/maillot/neutre.png" style="/* filter: none; */"></image>
+                            <g _ngcontent-ljs-c21="" id="drag_j0" xmlns:svg="http://www.w3.org/2000/svg" class="dropzone draggable">
+                                <rect _ngcontent-ljs-c21="" fill="#21316a" height="39" rx="10" ry="10" width="80" x="-5" y="69"></rect>
+                                <text  fill="#ffffff" id="j0" left="0" top="0" transform="matrix(1,0,0,1,-7.5, 97)" class="text_joueur" style="font-size: 24px; font-family: Arial; text-anchor: start;"></text>
+                             </g>
+                        </g>
+
+                        <g _ngcontent-ljs-c23="" id="g_j0" transform="matrix(1,0,0,1,300,100)" app-goalkeeper-field="" class="gardien g_player" _nghost-ljs-c21="">
+                            <image _ngcontent-ljs-c21="" height="70" left="0" preserveAspectRatio="none" top="0" width="70" x="0" y="0" xmlns:svg="http://www.w3.org/2000/svg" class="m_gardien dropzone" xlink:href="https://www.demivolee.com/wp-content/plugins/OutilCompoWordpress-2.7.0/assets/maillot/neutre.png" style="/* filter: none; */"></image>
+                            <g _ngcontent-ljs-c21="" id="drag_j0" xmlns:svg="http://www.w3.org/2000/svg" class="dropzone draggable">
+                                <rect _ngcontent-ljs-c21="" fill="#21316a" height="39" rx="10" ry="10" width="80" x="-5" y="69"></rect>
+                                <text  fill="#ffffff" id="j0" left="0" top="0" transform="matrix(1,0,0,1,-7.5, 97)" class="text_joueur" style="font-size: 24px; font-family: Arial; text-anchor: start;"></text>
+                             </g>
+                        </g>
+
+
+                    </g>
+
+                </svg>
+
+
             </section>
         </main>
     </body>
